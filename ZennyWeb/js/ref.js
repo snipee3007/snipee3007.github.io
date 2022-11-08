613addcee802a8da6df971ee8ec5088d78166947
snipee3007 <102203715+snipee3007@users.noreply.github.com>
2022-11-08 23:22:51 +0700

    Add files via upload

---
+++ ZennyWeb/js/ref.js
@@ -0,0 +1,9 @@
+function mouseover() {
+    document.getElementById("haslink").style.color = "blue";
+}
+
+function mouseout() {
+    document.getElementById("haslink").style.color = "white";
+}
+
+
